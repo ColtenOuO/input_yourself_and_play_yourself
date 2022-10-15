5eb04ae0b860768266b9a857cd54c5550ce901d3
ColtenOuO <jun930436@gmail.com>
2022-10-16 03:15:50 +0800

    Eric is so dian...

---
+++ wordle/wordle/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace wordle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(209, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(352, 73);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "猜英文單字";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(184, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(405, 73);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "6 次猜錯機會";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(253, 209);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(273, 37);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "請輸入要猜的單字";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(247, 274);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(296, 31);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(288, 328);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(193, 61);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(34, 308);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(108, 37);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "時間:  ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 371);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(222, 37);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "猜錯次數: 0 次";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(253, 308);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(273, 37);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "請輸入要猜的單字";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(148, 308);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 37);
+            this.label7.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ wordle/wordle/Form1.cs
@@ -0,0 +1,318 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace wordle
+{
+    public partial class Form1 : Form
+    {
+        Button[] arr = new Button[26];
+        string answer = "";
+
+        int wrong = 0,now_time = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            label4.Visible = false;
+            label5.Visible = false;
+            label6.Visible = false;
+
+            now_time = 0;
+            timer1.Interval = 1000;
+            timer1.Enabled = false;
+        }
+        public void init()
+        {
+            textBox1.Text = "";
+
+
+            wrong = now_time = 0;
+            label5.Text = "猜錯次數: " + wrong + " 次";
+            timer1.Enabled = false;
+
+            for (int i=0;i<13;i++)
+            {
+                arr[i] = new Button();
+                arr[i].SetBounds(40 * (i + 1), 30, 40, 40);
+                arr[i].Enabled = false;
+                Controls.Add(arr[i]);
+                string s = "";
+                char u2 = (char)('A' + i);
+                s += u2;
+                arr[i].Text = s;
+
+                int idx = i;
+
+              //  arr[i].Click += (sender, ex) => arr_Click(arr[idx]);
+            }
+            for(int i=13;i<26;i++)
+            {
+                arr[i] = new Button();
+                arr[i].SetBounds(40 * (i - 13 + 1),70, 40, 40);
+                Controls.Add(arr[i]);
+                arr[i].Enabled = false;
+                string s = "";
+                char u2 = (char)('A' + i);
+                s += u2;
+                arr[i].Text = s;
+
+                int idx = i;
+
+              //  arr[i].Click += (sender, ex) => arr_Click(arr[idx]);
+            }
+
+            string u = "";
+            for (int i = 0; i < answer.Length; i++) u += "_ ";
+            label6.Text = u;
+
+            button1.Visible = false;
+            label1.Visible = false;
+            label2.Visible = false;
+            label3.Visible = false;
+            textBox1.Visible = false;
+            button1.Enabled = false;
+
+            label4.Visible = true;
+            label5.Visible = true;
+            label6.Visible = true;
+
+            label4.Text = "時間: 0";
+
+            return;
+        }
+        public void reset()
+        {
+            label4.Text = "時間: 0";
+            textBox1.Text = "";
+            timer1.Enabled = false;
+
+            textBox1.Enabled = true;
+            button1.Visible = true;
+            label1.Visible = true;
+            label2.Visible = true;
+            label3.Visible = true;
+            textBox1.Visible = true;
+
+            button1.Enabled = true;
+            textBox1.Enabled = true;
+
+            label4.Visible = false;
+            label5.Visible = false;
+            label6.Visible = false;
+
+            for (int i = 0; i < 26; i++) arr[i].Visible = false;
+
+            return;
+        }
+
+        public void Form1_KeyDown(object sender, KeyEventArgs e)        {
+            if (timer1.Enabled == false) return;
+
+            char u = '@';
+            switch (e.KeyCode)
+            {
+                case Keys.A:
+                    u = 'A';
+                    break;
+                case Keys.B:
+                    u = 'B';
+                    break;
+                case Keys.C:
+                    u = 'C';
+                    break;
+                case Keys.D:
+                    u = 'D';
+                    break;
+                case Keys.E:
+                    u = 'E';
+                    break;
+                case Keys.F:
+                    u = 'F';
+                    break;
+                case Keys.G:
+                    u = 'G';
+                    break;
+                case Keys.H:
+                    u = 'H';
+                    break;
+                case Keys.I:
+                    u = 'I';
+                    break;
+                case Keys.J:
+                    u = 'J';
+                    break;
+                case Keys.K:
+                    u = 'K';
+                    break;
+                case Keys.L:
+                    u = 'L';
+                    break;
+                case Keys.M:
+                    u = 'M';
+                    break;
+                case Keys.N:
+                    u = 'N';
+                    break;
+                case Keys.O:
+                    u = 'O';
+                    break;
+                case Keys.P:
+                    u = 'P';
+                    break;
+                case Keys.Q:
+                    u = 'Q';
+                    break;
+                case Keys.R:
+                    u = 'R';
+                    break;
+                case Keys.S:
+                    u = 'S';
+                    break;
+                case Keys.T:
+                    u = 'T';
+                    break;
+                case Keys.U:
+                    u = 'U';
+                    break;
+                case Keys.V:
+                    u = 'V';
+                    break;
+                case Keys.W:
+                    u = 'W';
+                    break;
+                case Keys.X:
+                    u = 'X';
+                    break;
+                case Keys.Y:
+                    u = 'Y';
+                    break;
+                case Keys.Z:
+                    u = 'Z';
+                    break;
+                defalult:
+                    break;
+            }
+
+            textBox1.Text = "";
+
+            solve(u);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            answer = "";
+            for(int i=0;i<textBox1.Text.Length;i++)
+            {
+                if( textBox1.Text[i] >= 'a' && textBox1.Text[i] <= 'z' )
+                {
+                    char u = (char)(textBox1.Text[i] - ('a' - 'A'));
+                    answer += u;
+                }
+                else
+                {
+                    answer += textBox1.Text[i];
+                }
+            }
+            init();
+
+            timer1.Enabled = true;
+            for (int i = 0; i < 26; i++) arr[i].Visible = true;
+        }
+        private void solve(char u)
+        {
+            char s = u;
+
+            if (s >= 'a' && s <= 'z') s = (char)(s - ('a' - 'A'));
+
+            bool have = false;
+            string s2 = "";
+            for(int i=0;i<answer.Length;i++)
+            {
+                if (answer[i] == s)
+                {
+                    have = true;
+                    s2 += s;
+                    arr[s-'A'].BackColor = Color.LightGreen;
+                }
+                else s2 += label6.Text[i*2];
+
+                s2 += " ";
+            }
+
+            label6.Text = s2;
+
+            if( have == false )
+            {
+                arr[s - 'A'].Visible = false;
+                wrong++;
+            }
+
+            label5.Text = "猜錯次數: " + wrong + " 次";
+
+            if( wrong >= 6 )
+            {
+                MessageBox.Show("你輸了");
+                timer1.Enabled = false;
+                reset();
+            }
+            else
+            {
+                bool win = true;
+                for(int i=0;i<label6.Text.Length;i++)
+                {
+                    if (label6.Text[i] == '_') win = false;
+                }
+
+                if( win == true )
+                {
+                    MessageBox.Show("花費時間:" + now_time + "\n猜錯 " + wrong + "次", "You win!");
+                    timer1.Enabled = false;
+                    reset();
+                }
+            }
+        }
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e) // 247,274
+        {
+            char c = e.KeyChar;
+            bool ok = false;
+
+            if (c >= 'a' && c <= 'z') ok = true;
+            if (c >= 'A' && c <= 'Z') ok = true;
+
+            if (ok == true)
+            {
+                e.Handled = false;
+                if (timer1.Enabled == true)
+                {
+                    MessageBox.Show("你贏了", "You win!");
+                    char u = c;
+                    solve(c);
+                }
+            }
+            else
+            {
+                e.Handled = true;
+            }
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int num = ++now_time;
+            label4.Text = "時間: " + num.ToString();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.KeyPreview = true;
+        }
+
+    }
+}
